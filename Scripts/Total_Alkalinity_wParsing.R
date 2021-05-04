4575520757590b873a180be3063767eea4ccce8e
daniellembecker <danielle_becker@uri.edu>
2021-05-04 11:45:54 -0400

    E5 initial test samples and blue tank titrations 0504 dmb

--- Scripts/Total_Alkalinity_wParsing.R
+++ Scripts/Total_Alkalinity_wParsing.R
@@ -29,13 +29,13 @@ library(seacarb) #used to calculate TA
 library(tidyverse)
 
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
-path<-"Data/Bailout_BlueTank_Titrations/20210428/" #the location of all your titration files, your folder of the day!
-massfile<-"Mass_20210428.csv" # name of your file with masses
-titrationfile<-'Titrations-4_28_2021-PutnamLab.csv'# name of the last titration file run
+path<-"Data/E5_Titrations/20210504_E5_Initial_Sample_Test/" #the location of all your titration files, your folder of the day!
+massfile<-"Mass_20210504.csv" # name of your file with masses
+titrationfile<-'Titrations-5_4_2021-PutnamLab.csv'# name of the last titration file run
 
 
 # Date that the data were run
-date<-'20210428'
+date<-'20210504'
 
 #DO NOT CHANGE ANYTHING BELOW THIS LINE UNLESS A NEW BOTTLE OF ACID IS USED
 
@@ -212,3 +212,4 @@ cumu.data <- read.csv("Data/Cumulative_TA_Output.csv", header=TRUE, sep=",")
 update.data <- rbind(cumu.data, TA)
 
 write.table(update.data,"Data/Cumulative_TA_Output.csv",sep=",", row.names=FALSE)
+

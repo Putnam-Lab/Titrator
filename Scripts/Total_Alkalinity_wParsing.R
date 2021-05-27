781d2500cc81405dea806e301e4d8694c6665e2f
daniellembecker <danielle_becker@uri.edu>
2021-05-27 12:15:53 -0400

    completed rest of run 3 sampples for March 2020 TP2, processed 5 samples from run 4 for TP2

--- Scripts/Total_Alkalinity_wParsing.R
+++ Scripts/Total_Alkalinity_wParsing.R
@@ -29,13 +29,13 @@ library(seacarb) #used to calculate TA
 library(tidyverse)
 
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
-path<-"Data/E5_Titrations/E5_TP2_Sample_Processing_Summer2021_DMB/20210526_E5_March2020_Run2_Run3/Run2_4_Samples_Titration1/" #the location of all your titration files, your folder of the day!
-massfile<-"Mass_20210526.csv" # name of your file with masses
-titrationfile<-'Titrations-5_26_2021-PutnamLab.csv'# name of the last titration file run
+path<-"Data/E5_Titrations/E5_TP2_Sample_Processing_Summer2021_DMB/20210526_E5_March2020_Run3_Run4/" #the location of all your titration files, your folder of the day!
+massfile<-"Mass_20210527.csv" # name of your file with masses
+titrationfile<-'Titrations-5_27_2021-PutnamLab.csv'# name of the last titration file run
 
 
 # Date that the data were run
-date<-'20210526'
+date<-'20210527'
 
 #DO NOT CHANGE ANYTHING BELOW THIS LINE UNLESS A NEW BOTTLE OF ACID IS USED
 

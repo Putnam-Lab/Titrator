8342d0180900c8f64983197a68aada8cc01eb1f0
KristenDoyle <Kristen_doyle8@uri.edu>
2022-01-31 13:53:38 -0500

    DMBP ran titrations for cbls wetlab for another 12pm timepoint

--- Scripts/Total_Alkalinity_wParsing.R
+++ Scripts/Total_Alkalinity_wParsing.R
@@ -30,13 +30,13 @@ library(seacarb) #used to calculate TA
 library(tidyverse)
 
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
-path<-"Data/BlueTank_Titrations/20220127/" #the location of all your titration files, your folder of the day!
-massfile<-"Mass_20220127.csv" # name of your file with masses
-titrationfile<-'20220127_BlueTankTitrations_PutnamLab.csv'# name of the last titration file run
+path<-"Data/BlueTank_Titrations/20220131/" #the location of all your titration files, your folder of the day!
+massfile<-"Mass_20220131.csv" # name of your file with masses
+titrationfile<-'20220131_BlueTankTitrations_PutnamLab.csv'# name of the last titration file run
 
 
 # Date that the data were run
-date<-'20220127'
+date<-'20220131'
 
 #DO NOT CHANGE ANYTHING BELOW THIS LINE UNLESS A NEW BOTTLE OF ACID IS USED
 

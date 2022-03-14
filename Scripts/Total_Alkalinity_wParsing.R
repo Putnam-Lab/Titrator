12e30c98a15e9940bb6320efa92ceb49efab25a6
KristenDoyle <kristen_doyle8@uri.edu>
2022-03-14 17:37:47 -0400

    export method and 20200314 titrations

--- Scripts/Total_Alkalinity_wParsing.R
+++ Scripts/Total_Alkalinity_wParsing.R
@@ -30,13 +30,13 @@ library(seacarb) #used to calculate TA
 library(tidyverse)
 
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
-path<-"Data/E5_Titrations/E5_2020_Calcification_Samples/20220310" #the location of all your titration files, your folder of the day!
-massfile<-"Mass_Run2_20220310.csv" # name of your file with masses
-titrationfile<-'20220310_Run2_PutnamTitrations_PutnamLab.csv'# name of the last titration file run
+path<-"Data/E5_Titrations/E5_2020_Calcification_Samples/20220314" #the location of all your titration files, your folder of the day!
+massfile<-"Mass_Run1_20220314.csv" # name of your file with masses
+titrationfile<-'20220314_Run1_PutnamTitrations_PutnamLab.csv'# name of the last titration file run
 
 
 # Date that the data were run
-date<-'20220310'
+date<-'20220314'
 
 #DO NOT CHANGE ANYTHING BELOW THIS LINE UNLESS A NEW BOTTLE OF ACID IS USED
 

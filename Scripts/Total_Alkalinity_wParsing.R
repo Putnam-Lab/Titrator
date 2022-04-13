b421889170c6134f33518e72342261cdf95b828e
KristenDoyle <kristen_doyle8@uri.edu>
2022-04-13 12:16:06 -0400

    completed tank titrations 0413

--- Scripts/Total_Alkalinity_wParsing.R
+++ Scripts/Total_Alkalinity_wParsing.R
@@ -30,13 +30,13 @@ library(seacarb) #used to calculate TA
 library(tidyverse)
 
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
-path<-"Data/E5_Titrations/E5_2020_Calcification_Samples/20220407" #the location of all your titration files, your folder of the day!
-massfile<-"Mass_Run3_20220407.csv" # name of your file with masses
-titrationfile<-'20220407_Run3_PutnamLab.csv'# name of the last titration file run
+path<-"Data/BlueTank_Titrations/20220413" #the location of all your titration files, your folder of the day!
+massfile<-"Mass_20220413.csv" # name of your file with masses
+titrationfile<-'20220413_PutnamLab.csv'# name of the last titration file run
 
 
 # Date that the data were run
-date<-'20220407'
+date<-'20220413'
 
 #DO NOT CHANGE ANYTHING BELOW THIS LINE UNLESS A NEW BOTTLE OF ACID IS USED
 

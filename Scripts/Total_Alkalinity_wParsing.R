45cd9349d14ff28882fcbd7bc010819bd41fc228
Amy Zyck <amaelia_zyck@uri.edu>
2022-06-08 15:48:39 -0400

    titrator training dmbp and lz, changed new acid titrant bottle, blue tank titrations dmbp 0608

--- Scripts/Total_Alkalinity_wParsing.R
+++ Scripts/Total_Alkalinity_wParsing.R
@@ -30,13 +30,13 @@ library(seacarb) #used to calculate TA
 library(tidyverse)
 
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
-path<-"Data/BlueTank_Titrations/20220518" #the location of all your titration files, your folder of the day!
-massfile<-"Mass_20220518.csv" # name of your file with masses
-titrationfile<-'20220518_PutnamLab.csv'# name of the last titration file run
+path<-"Data/BlueTank_Titrations/20220608/" #the location of all your titration files, your folder of the day!
+massfile<-"Mass_20220608.csv" # name of your file with masses
+titrationfile<-'20220608_PutnamLabPutnamLab.csv'# name of the last titration file run
 
 
 # Date that the data were run
-date<-'20220518'
+date<-'20220608'
 
 #DO NOT CHANGE ANYTHING BELOW THIS LINE UNLESS A NEW BOTTLE OF ACID IS USED
 

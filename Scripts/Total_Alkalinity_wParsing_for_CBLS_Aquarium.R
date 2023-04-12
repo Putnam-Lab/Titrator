0fdae47741bfcc62a939aafbbe485020536db5b5
laurenzane <laurenzane99@gmail.com>
2023-04-12 13:17:10 -0400

    lz CBLS aquarium titrations

--- Scripts/Total_Alkalinity_wParsing_for_CBLS_Aquarium.R
+++ Scripts/Total_Alkalinity_wParsing_for_CBLS_Aquarium.R
@@ -34,13 +34,13 @@ library(tidyverse)
 
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
 
-path<-"Data/BlueTank_Titrations/20230308/" #the location of all your titration files, your folder of the day!
-massfile<-"Mass_20230308.csv" # name of your file with masses
-titrationfile<-'20230308_LZ.CSV'# name of the last titration file run
+path<-"Data/BlueTank_Titrations/20230412/" #the location of all your titration files, your folder of the day!
+massfile<-"Mass_20230412.csv" # name of your file with masses
+titrationfile<-'20230412_LZ.CSV'# name of the last titration file run
 
 
 # Date that the data were run
-date<-'20230308'
+date<-'20230412'
 
 
 #DO NOT CHANGE ANYTHING BELOW THIS LINE UNLESS A NEW BOTTLE OF ACID IS USED

82fad96d99951d0af4a3098a3987574afabae235
daniellembecker <danielle_becker@uri.edu>
2024-02-29 15:54:25 -0500

    data for titrations 229

--- Scripts/Total_Alkalinity_wParsing.R
+++ Scripts/Total_Alkalinity_wParsing.R
@@ -39,8 +39,8 @@ library(tidyverse)
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
 
 ## <<<<<<< HEAD
-massfile<-"Mass_20240229_CRM.csv" # name of your file with masses
-titrationfile<-'20240229_cbls_CRM.csv'# name of the last titration file run
+massfile<-"Mass_20240229_samples.csv" # name of your file with masses
+titrationfile<-'20240229_cbls_samples.csv'# name of the last titration file run
 date<-'20240229' #date that data was run
 path<-"../Data/BlueTank_Titrations/20240229/" #the location of all your titration files, your folder of the day!
 

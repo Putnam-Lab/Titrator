68e091d46a6a5740d622f1c104a2694feee5ce27
sscott21 <sscott21@uri.edu>
2024-10-01 18:49:25 -0400

    Titrations from 20241001

--- Scripts/Total_Alkalinity_wParsing.R
+++ Scripts/Total_Alkalinity_wParsing.R
@@ -42,8 +42,8 @@ library(tidyverse)
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
 
 ## <<<<<<< HEAD
-massfile<-"Mass_20241001_CRM.csv" # name of your file with masses
-titrationfile<-'20241001_CBLS_CRM.csv'# name of the last titration file run
+massfile<-"Mass_20241001_Samples.csv" # name of your file with masses
+titrationfile<-'20241001_CBLS_Samples.csv'# name of the last titration file run
 date<-'20241001' #date that data was run
 path<-"C:/Users/swimg/OneDrive/Desktop/Putnam-Lab/Titrator/Data/BlueTank_Titrations/20241001" #the location of all your titration files, your folder of the day!
 

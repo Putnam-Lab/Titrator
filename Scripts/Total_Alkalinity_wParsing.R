17ac678753fef0fd0829552d1365ca412c4a4522
flofields <ffields@uri.edu>
2025-07-03 16:29:05 -0400

    updating titrator data

--- Scripts/Total_Alkalinity_wParsing.R
+++ Scripts/Total_Alkalinity_wParsing.R
@@ -42,10 +42,10 @@ library(tidyverse)
 #CHANGE THESE VALUES EVERY DAY----------------------------------------------
 
 ## <<<<<<< HEAD
-massfile<-"Mass_20250618_Samples.csv" # name of your file with masses
-titrationfile<-'20250618_CBLS_Samples.csv'# name of the last titration file run
-date<-'20250618' #date that data was run
-path<-"Data/BlueTank_Titrations/20250618" #the location of all your titration files, your folder of the day!
+massfile<-"Mass_20250703_CRM.csv" # name of your file with masses
+titrationfile<-'20250703_CBLS_CRM.csv'# name of the last titration file run
+date<-'20250703' #date that data was run
+path<-"Data/BlueTank_Titrations/20250703" #the location of all your titration files, your folder of the day!
 
 
 
